a9fb3c4121caf6750f8d1b818c85327a0a755ad2
Phillipe <ge.bonhomme@duocuc.cl>
2026-05-13 13:24:33 -0400

    Fix a mala configuracion de SecurityConfig.java en micro Productos

--- Productos/src/main/java/com/smartlogix/Productos/config/SecurityConfig.java
+++ Productos/src/main/java/com/smartlogix/Productos/config/SecurityConfig.java
@@ -2,9 +2,15 @@ package com.smartlogix.Productos.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
 @EnableMethodSecurity
@@ -16,9 +22,22 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
-                );
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService users() {
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("{noop}1234")
+                .roles("ADMINISTRADOR")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin);
     }
 }
