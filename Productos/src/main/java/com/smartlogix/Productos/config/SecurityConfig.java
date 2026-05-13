33bef2feb17b50704097a5439afe1049a5e1ad57
Phillipe <ge.bonhomme@duocuc.cl>
2026-05-13 16:57:51 -0400

    BFF Funcionando | Fix a seguridad de Productos

--- Productos/src/main/java/com/smartlogix/Productos/config/SecurityConfig.java
+++ Productos/src/main/java/com/smartlogix/Productos/config/SecurityConfig.java
@@ -2,42 +2,46 @@ package com.smartlogix.Productos.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.smartlogix.Productos.security.JwtFilter;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    private final JwtFilter jwtFilter;
 
-        http
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http
+    ) throws Exception {
+
+        return http
                 .csrf(csrf -> csrf.disable())
+
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(
+                                SessionCreationPolicy.STATELESS
+                        )
+                )
+
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().authenticated()
                 )
-                .httpBasic(Customizer.withDefaults());
 
-        return http.build();
-    }
+                .addFilterBefore(
+                        jwtFilter,
+                        UsernamePasswordAuthenticationFilter.class
+                )
 
-    @Bean
-    public UserDetailsService users() {
-
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password("{noop}1234")
-                .roles("ADMINISTRADOR")
                 .build();
-
-        return new InMemoryUserDetailsManager(admin);
     }
 }
