7a6189e196d10a4f95d127d3c8a0b0aefd3a8cf1
Phillipe <ge.bonhomme@duocuc.cl>
2026-05-17 22:21:57 -0400

    Agregado todos los metodos de Producto Controller al BFF

--- bff/src/main/java/com/smartlogix/bff/controller/ProductosBffController.java
+++ bff/src/main/java/com/smartlogix/bff/controller/ProductosBffController.java
@@ -1,9 +1,10 @@
 package com.smartlogix.bff.controller;
 
-import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.smartlogix.bff.client.ProductosClient;
+import com.smartlogix.bff.dto.PageResponseDTO;
 import com.smartlogix.bff.dto.ProductoRequestDTO;
 import com.smartlogix.bff.dto.ProductoResponseDTO;
 
@@ -27,12 +29,14 @@ public class ProductosBffController {
     }
 
     @GetMapping
-    public Page<ProductoResponseDTO> listar(
+    public PageResponseDTO<ProductoResponseDTO> listar(
+            @RequestHeader("Authorization") String token,
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "20") int size
     ) {
 
         return productosClient.listarProductos(
+                token,
                 page,
                 size
         );
@@ -40,10 +44,11 @@ public class ProductosBffController {
 
     @GetMapping("/{sku}")
     public ProductoResponseDTO obtener(
+                @RequestHeader("Authorization") String token,
             @PathVariable String sku
     ) {
 
-        return productosClient.obtenerPorSku(sku);
+        return productosClient.obtenerPorSku(token, sku);
     }
 
     @PostMapping
@@ -55,6 +60,61 @@ public class ProductosBffController {
         return productosClient.crearProducto(
                 token,
                 dto
+        );
+    }
+
+    @GetMapping("/buscar/nombre")
+    public PageResponseDTO<ProductoResponseDTO> buscarPorNombre(
+                @RequestHeader("Authorization") String token,
+            @RequestParam String nombre,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "20") int size
+    ) {
+
+        return productosClient.buscarPorNombre(
+                token,
+                nombre,
+                page,
+                size
+        );
+    }
+
+     @PutMapping("/{sku}")
+    public ProductoResponseDTO actualizar(
+            @RequestHeader("Authorization") String token,
+            @PathVariable String sku,
+            @RequestBody ProductoRequestDTO dto
+    ) {
+
+        return productosClient.actualizarProducto(
+                token,
+                sku,
+                dto
+        );
+    }
+
+    @DeleteMapping("/{sku}")
+    public void eliminar(
+            @RequestHeader("Authorization") String token,
+            @PathVariable String sku
+    ) {
+
+        productosClient.eliminarProducto(token, sku);
+    }
+
+     @GetMapping("/buscar/categoria")
+    public PageResponseDTO<ProductoResponseDTO> listarPorCategoria(
+            @RequestHeader("Authorization") String token,
+            @RequestParam String categoria,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "20") int size
+    ) {
+
+        return productosClient.listarPorCategoria(
+                token,
+                categoria,
+                page,
+                size
         );
     }
 }
