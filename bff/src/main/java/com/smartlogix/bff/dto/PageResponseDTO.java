2a990021393730ca19b45ad6b81ad094aa43f55f
Phillipe <ge.bonhomme@duocuc.cl>
2026-05-19 15:59:51 -0400

    Agregada funciones de Usuario a controller del BFF

--- bff/src/main/java/com/smartlogix/bff/dto/PageResponseDTO.java
+++ bff/src/main/java/com/smartlogix/bff/dto/PageResponseDTO.java
@@ -7,13 +7,12 @@ public class PageResponseDTO<T> {
     private List<T> content;
 
     private int page;
-
     private int size;
 
     private long totalElements;
-
     private int totalPages;
 
+    private boolean first;
     private boolean last;
 
     public List<T> getContent() {
@@ -54,6 +53,14 @@ public class PageResponseDTO<T> {
 
     public void setTotalPages(int totalPages) {
         this.totalPages = totalPages;
+    }
+
+    public boolean isFirst() {
+        return first;
+    }
+
+    public void setFirst(boolean first) {
+        this.first = first;
     }
 
     public boolean isLast() {
---
+++ bff/src/main/java/com/smartlogix/bff/dto/UsuarioRequestDTO.java
@@ -0,0 +1,44 @@
+package com.smartlogix.bff.dto;
+
+public class UsuarioRequestDTO {
+
+    private String nombre;
+
+    private String email;
+
+    private String contrasena;
+
+    private String rol;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+}
